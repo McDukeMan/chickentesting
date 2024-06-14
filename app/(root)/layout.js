817d5ff60516ff03df0f7345e95586a7f5e58ca0
mcdukeman <mcdukemannguyen131001@gmail.com>
2024-06-14 09:32:26 +0700

    bug fix

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -8,8 +8,8 @@ import RightSideBar from "@components/layout/RightSideBar";
 import BottomBar from "@components/layout/BottomBar";
 
 export const metadata = {
-  title: "Vibe Zone",
-  description: "Next 14 Social Media App",
+  title: "Chicken Social",
+  description: "Next.js",
 };
 
 const inter = Inter({ subsets: ["latin"] });
